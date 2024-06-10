21f8ab7499ecc64177070814e36fadf919f3a773
NiNinanana <yeen00123@gmail.com>
2024-06-10 15:51:40 +0900

    feat: 현재 위치의 행정동 구하는 api 추가

---
+++ src/getLegalDongFromCoords.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { Request, Response } from "express";
+
+export const getLegalDongFromCoords = async (req: Request, res: Response) => {
+  const clientId = process.env.NCP_CLIENT_ID;
+  const clientSecret = process.env.NCP_CLIENT_SECRET;
+
+  const { coords } = req.query;
+
+  if (!coords) {
+    return res.status(400).json({ error: "Coords parameter is required" });
+  }
+
+  try {
+    const url = `https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?coords=${coords}&orders=legalcode&output=json`;
+
+    const { data } = await axios.get(url, {
+      headers: {
+        "X-NCP-APIGW-API-KEY-ID": clientId,
+        "X-NCP-APIGW-API-KEY": clientSecret,
+      },
+    });
+
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Failed to fetch legalDong data");
+  }
+};
